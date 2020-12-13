10a43b7cb37b6fbb2fe588c51d2d23772f8b6fb0
Lucy McGowan <lucydagostino@gmail.com>
2020-12-13 08:58:22 -0500

    :sparkles:

---
+++ R/get_prob_missed_infection.R
@@ -0,0 +1,50 @@
+#' Calculate the probability of a missed infection given a test + quarantine
+#' strategy
+#'
+#' @param test_time Numeric. Date post-exposure test sample is collected
+#' @param additional_quarantine_time Numeric. Additional days quarantined after
+#'   test sample is collected. Default is 0
+#' @param tolerance Numeric. Tolerance. Default is 0.01
+#' @param shape_exposure_to_threshold Numeric. Shape of the gamma distribution
+#'   that describes the time from exposure to crossing the threshold of
+#'   detection. Default is 2.08.
+#' @param shape_threshold_to_symptoms Numeric. Shape of the gamma distribution
+#'   that describes the time from crossing the threshold of
+#'   detection to symptom onset. Default is 2.05.
+#' @param rate Numeric. Rate of the gamma distributions that describe the time
+#'   from exposure to crossing the threshold of detection and the time from
+#'   crossing the threshold of detection to symptom onset. Default is 0.72.
+#'
+#' @export
+#'
+#' @examples
+#' ## test sample collected 5 days post-exposure, quarantine until day 5
+#' get_prob_missed_infection(5)
+#'
+#' ## test sample collected 7 days post-exposure, quarantine until day 9
+#' get_prob_missed_infection(7, 2)
+get_prob_missed_infection <- function(test_time,
+                                      additional_quarantine_time = 0,
+                                      tolerance = 0.01,
+                                      shape_exposure_to_threshold = 2.08,
+                                      shape_threshold_to_symptoms = 2.05,
+                                      rate = 0.72) {
+  integrate(top, 0, Inf, t = test_time, s = additional_quarantine_time,
+            shape_y = shape_threshold_to_symptoms,
+            shape_x = shape_exposure_to_threshold,
+            rate = rate,
+            rel.tol = tolerance)$value
+}
+
+top <- function(x, t, s, shape_y, shape_x, rate) {
+  (purrr::map_dbl(t - x, get_fnr) *
+     (1 - G((t + s) - x, shape_y, rate)) * f(x, shape_x, rate))
+}
+
+G <- function(y, shape, rate) {
+  pgamma(y, shape, rate)
+}
+
+f <- function(x, shape, rate) {
+  dgamma(x, shape, rate)
+}
---
+++ R/get_sensitivity.R
@@ -0,0 +1,31 @@
+#' Get test sensitivity x days from crossing the threshold of detection
+#'
+#' @param time Numeric. Number of days from crossing the threshold of detection
+#' @param shape Numeric. Shape of the gamma distribution used to estimate
+#'   test sensitivity. Default is 2.32
+#' @param rate Numeric. Rate of the gamma distribution used to estimate test
+#'   sensitivity. Default is 0.23
+#'
+#' @export
+#'
+#' @examples
+#' get_sensitivity(1)
+get_sensitivity <- function(time, shape = 2.32, rate = 0.23) {
+  dgamma(time, shape, rate) / dgamma((shape - 1) / rate, shape, rate) * 0.99
+}
+
+#' Get test false negative rate x days from crossing the threshold of detection
+#'
+#' @param time Numeric. Number of days from crossing the threshold of detection
+#' @param shape Numeric. Shape of the gamma distribution used to estimate
+#'   test sensitivity. Default is 2.32
+#' @param rate Numeric. Rate of the gamma distribution used to estimate test
+#'   sensitivity. Default is 0.23
+#'
+#' @export
+#'
+#' @examples
+#' get_fnr(1)
+get_fnr <- function(time, shape = 2.32, rate = 0.23) {
+  1 - get_sensitivity(time, shape, rate)
+}
