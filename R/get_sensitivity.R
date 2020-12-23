5daf21f6edbd17d9436de758c8d579e10adcdbf6
Lucy McGowan <lucydagostino@gmail.com>
2020-12-23 10:06:42 -0500

    add ability to change max sensitivity

--- R/get_sensitivity.R
+++ R/get_sensitivity.R
@@ -5,14 +5,16 @@
 #'   test sensitivity. Default is 2.33
 #' @param rate Numeric. Rate of the gamma distribution used to estimate test
 #'   sensitivity. Default is 0.24
+#' @param max_sensitivity Numeric. Value between 0 and 1 indicating the maximum
+#'   sensitivity. Default is 0.99.
 #'
 #' @export
 #'
 #' @examples
 #' get_sensitivity(1)
-get_sensitivity <- function(time, shape = 2.33, rate = 0.24) {
+get_sensitivity <- function(time, shape = 2.33, rate = 0.24, max_sensitivity = 0.99) {
   stats::dgamma(time, shape, rate) /
-    stats::dgamma((shape - 1) / rate, shape, rate) * 0.99
+    stats::dgamma((shape - 1) / rate, shape, rate) * max_sensitivity
 }
 
 #' Get test false negative rate x days from crossing the threshold of detection
@@ -22,11 +24,13 @@ get_sensitivity <- function(time, shape = 2.33, rate = 0.24) {
 #'   test sensitivity. Default is 2.33
 #' @param rate Numeric. Rate of the gamma distribution used to estimate test
 #'   sensitivity. Default is 0.24
+#' @param max_sensitivity Numeric. Value between 0 and 1 indicating the maximum
+#'   sensitivity. Default is 0.99.
 #'
 #' @export
 #'
 #' @examples
 #' get_fnr(1)
-get_fnr <- function(time, shape = 2.33, rate = 0.24) {
-  1 - get_sensitivity(time, shape, rate)
+get_fnr <- function(time, shape = 2.33, rate = 0.24, max_sensitivity = 0.99) {
+  1 - get_sensitivity(time, shape, rate, max_sensitivity)
 }
